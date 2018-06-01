3a5c015c595fec57048c639c1d93b110e4854741
Tamara Ambo <tamara1819@yahoo.com>
2018-06-01 16:57:42 +0200

    dodat  rekord u tabeli

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
 insert into predmet(id,ime,opis) values(20001,'Ime1','Opis1');
 insert into predmet(id,ime,opis) values(20002,'Ime2','Opis2');
 insert into predmet(id,ime,opis) values(20003,'Ime3','Opis3');
+insert into predmet(id,ime,opis) values(20004,'Ime4','Opis5');
 
